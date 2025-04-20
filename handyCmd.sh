56132de29de6a7afd4869e924d674dc4e481f25e
RodenLuo <roden.luo@gmail.com>
2025-04-20 14:48:14 +0300

    Auto: Add new handyCmd.sh script for development build and remote control commands.

---
+++ handyCmd.sh
@@ -0,0 +1,5 @@
+devel build /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle; devel install /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle exit true
+
+remotecontrol rest start port 45145
+
+remotecontrol rest stop
