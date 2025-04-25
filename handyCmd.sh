123bf5450a661cf2f10e855efb6fd5a2bbdf5e2e
RodenLuo <roden.luo@gmail.com>
2025-04-25 11:37:08 +0300

    Auto: Update handyCmd.sh to include toolshed uninstall command before build and install steps

--- handyCmd.sh
+++ handyCmd.sh
@@ -1,4 +1,4 @@
-devel build /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle; devel install /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle exit true
+toolshed uninstall proteincraft; devel build /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle; devel install /home/luod/ProteinCraft/ProteinCraft-ChimeraX_bundle exit true
 
 remotecontrol rest start port 45145
 
