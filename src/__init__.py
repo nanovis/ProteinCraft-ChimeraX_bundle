947d12dd9d236b4811bcbc4daf2d42b83d268a5f
RodenLuo <roden.luo@gmail.com>
2025-04-25 22:16:01 +0300

    register flankingEnabled command

--- src/__init__.py
+++ src/__init__.py
@@ -93,6 +93,9 @@ class _ProteinCraftAPI(BundleAPI):
         elif ci.name == "proteincraft flankingNum":
             func = cmd.flankingNum
             desc = cmd.flankingNum_desc
+        elif ci.name == "proteincraft flankingEnabled":
+            func = cmd.flankingEnabled
+            desc = cmd.flankingEnabled_desc
         else:
             raise ValueError(f"trying to register unknown command: {ci.name}")
 
